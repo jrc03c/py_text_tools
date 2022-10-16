e3d9342109de180e1fd146deacbd2fb514d9de59
jrc03c <jrc03c@pm.me>
2022-10-16 16:53:17 -0500

    ...

--- tests/test_strip.py
+++ tests/test_strip.py
@@ -13,3 +13,28 @@
 
 #   expect(strip("I don't like you.")).toBe("i dont like you")
 # })
+
+from py_text_tools.helpers import strip
+from unittest import TestCase
+
+
+class StripTestCase(TestCase):
+    def test(self):
+        self.assertEqual(strip("foobarbaz"), "foobarbaz")
+
+        self.assertEqual(
+            strip("Hello, world! My name is Josh!"), "hello world my name is josh"
+        )
+
+        self.assertEqual(
+            strip("'42 is the number thou shalt count!'"),
+            "42 is the number thou shalt count",
+        )
+
+        self.assertEqual(strip("I don't like you."), "i dont like you")
+        self.assertEqual(strip("how about now"), "how about now")
+
+        self.assertEqual(
+            strip("heresAnotherOne_YesOrNo-orMaybeSo"),
+            "heresanotherone yesorno ormaybeso",
+        )
