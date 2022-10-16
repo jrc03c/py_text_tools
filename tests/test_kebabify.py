89fd19cd719fd357e99afbede2a7517b1c6347fd
jrc03c <jrc03c@pm.me>
2022-10-16 16:47:11 -0500

    ...

--- tests/test_kebabify.py
+++ tests/test_kebabify.py
@@ -1,17 +1,21 @@
-# const kebabify = require("./kebabify.js")
+from py_text_tools import kebabify
+from unittest import TestCase
 
-# test("tests `kebabify`", () => {
-#   expect(kebabify("foobarbaz")).toBe("foobarbaz")
 
-#   expect(kebabify("Hello, world! My name is Josh!")).toBe(
-#     "hello-world-my-name-is-josh"
-#   )
+class KebabifyTestCase(TestCase):
+    def test(self):
+        self.assertEqual(kebabify("foobarbaz"), "foobarbaz")
 
-#   expect(kebabify("'42 is the number thou shalt count!'")).toBe(
-#     "42-is-the-number-thou-shalt-count"
-#   )
+        self.assertEqual(
+            kebabify("Hello, world! My name is Josh!"), "hello-world-my-name-is-josh"
+        )
 
-#   expect(kebabify("I don't like you.")).toBe("i-dont-like-you")
+        self.assertEqual(
+            kebabify("'42 is the number thou shalt count!'"),
+            "42-is-the-number-thou-shalt-count",
+        )
 
-#   expect(kebabify("how-about-now")).toBe("how-about-now")
-# })
+        self.assertEqual(kebabify("I don't like you."), "i-dont-like-you")
+
+        self.assertEqual(kebabify("how-about-now"), "how-about-now")
+
