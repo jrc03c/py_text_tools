841e632f6568fcae1659a65ad84794bc8ff5705f
jrc03c <jrc03c@pm.me>
2022-10-16 16:21:48 -0500

    ...

--- py_text_tools/stringify.py
+++
@@ -1,5 +0,0 @@
-# const { copy } = require("@jrc03c/js-math-tools")
-
-# function stringify(x, replacer, space) {
-#   return JSON.stringify(copy(x), replacer, space)
-# }
--- py_text_tools/unindent.py
+++ py_text_tools/unindent.py
@@ -1,10 +1,28 @@
-# function unindent(text) {
-#   const lines = text.split("\n")
+import re
 
-#   const indentations = lines
-#     .filter(line => line.trim().length > 0)
-#     .map(line => line.split("").findIndex(char => !char.match(/\s/g)))
 
-#   const minIndentation = Math.min(...indentations)
-#   return lines.map(line => line.substring(minIndentation)).join("\n")
-# }
+def unindent(text):
+    assert type(text) == str, "`text` must be a string!"
+
+    def find_index(fn, x):
+        for i in range(0, len(x)):
+            if fn(x[i]):
+                return i
+
+        return None
+
+    whitespace = re.compile("\s")
+    lines = text.split("\n")
+    lines = list(filter(lambda line: len(line.strip()) > 0, lines))
+
+    indentations = list(
+        map(
+            lambda line: find_index(
+                lambda char: not whitespace.match(char), line.split("")
+            ),
+            lines,
+        )
+    )
+
+    min_indentation = min(indentations)
+    return ("\n").join(list(map(lambda line: line[min_indentation:], lines)))
