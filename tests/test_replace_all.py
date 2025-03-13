1f42da320298ef362906bb2534a9a2ea5bb04ea6
jrc03c <jrc03c@pm.me>
2025-03-13 17:55:46 -0500

    ...

--- tests/test_replace_all.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from py_text_tools.helpers import replace_all
-
-
-class ReplaceAllTestCase(TestCase):
-    def test(self):
-        self.assertEqual(replace_all("foobar", "o", "z"), "fzzbar")
-        self.assertEqual(replace_all("Hello, world!", "!", "?"), "Hello, world?")
